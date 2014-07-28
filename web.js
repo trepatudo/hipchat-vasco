3560b591935c1354dc5997d979b1079d0e3dedf9
Robert Bergman <robert.bergman@gmail.com>
2014-07-28 16:37:41 -0600

    Initial derivation from rbergman/ack

---
+++ web.js
@@ -0,0 +1,14 @@
+var ack = require('ac-koa').require('hipchat');
+var pkg = require('./package.json');
+var app = ack(pkg);
+
+var addon = app.addon()
+  .hipchat()
+  .allowRoom(true)
+  .scopes('send_notification');
+
+addon.webhook('room_enter', function *() {
+  yield this.roomClient.sendNotification('Hi, ' + this.sender.name + '!');
+});
+
+app.listen();

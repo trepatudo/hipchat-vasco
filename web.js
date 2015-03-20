ec9e886e47ff601b2d1cfeca3572f00d599a36ab
Marco Silva <dev@marcosilva.pt>
2015-03-20 15:21:37 +0000

    just a small change

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var ack = require('ac-koa').require('hipchat');
 var pkg = require('./package.json');
 var app = ack(pkg);
-
+ 
 var addon = app.addon()
   .hipchat()
   .allowRoom(true)
   .scopes('send_notification');
-
-addon.webhook('room_message', /^\/hello$/, function *() {
-  yield this.roomClient.sendNotification('Hi, ' + this.sender.name + '!');
+ 
+addon.webhook('room_message', /@VascoAfonso/, function *() {
+  yield this.roomClient.sendNotification('Please, don\'t ruin the toilet!');
 });
-
+ 
 app.listen();
